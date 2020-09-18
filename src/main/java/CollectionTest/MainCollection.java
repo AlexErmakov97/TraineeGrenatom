c0324eba57a3bae11100175f6887d5ed0a88a850
Alexander <alex.ermakov631@gmail.com>
2020-09-18 22:50:34 +0300

    Замена ключ-значение в hashmap

--- src/main/java/CollectionTest/MainCollection.java
+++ src/main/java/CollectionTest/MainCollection.java
@@ -4,8 +4,18 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import java.util.HashMap;
+
 public class MainCollection {
     public static void main(String[] args) throws Exception {
+        HashMap <Integer, String> hashMap = new HashMap<>();
+        hashMap.put(1,"SAS");
+        hashMap.put(3,"DAS");
+        hashMap.put(4,"PAR");
+        hashMap.put(30,"DER");
+        System.out.println(hashMap.toString());
+        System.out.println(TestHashMap.changeKeyToValue(hashMap).toString());
+
 //        org.openjdk.jmh.Main.main(args);
 
 //        final Options options = new OptionsBuilder()
@@ -14,11 +24,11 @@ public class MainCollection {
 //                .build();
 //        new Runner(options).run();
 
-        final Options options = new OptionsBuilder()
-                .include(CollectionBenchmark.class.getSimpleName())
-                .forks(1)
-                .build();
-        new Runner(options).run();
+//        final Options options = new OptionsBuilder()
+//                .include(CollectionBenchmark.class.getSimpleName())
+//                .forks(1)
+//                .build();
+//        new Runner(options).run();
 
 //        ArrayList<Integer> arrayList = new ArrayList<>();
 //        LinkedList<Integer> linkedList = new LinkedList<>();
--- src/main/java/CollectionTest/TestHashMap.java
+++ src/main/java/CollectionTest/TestHashMap.java
@@ -1,14 +1,13 @@
 package CollectionTest;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class TestHashMap {
-    public HashMap<Integer, String> hashMap;
 
-    public TestHashMap(HashMap<Integer, String> hashMap) {
-        this.hashMap = hashMap;
+    public static <K,V> HashMap<V,K> changeKeyToValue(Map<K,V> map){
+        HashMap<V,K> rev = new HashMap<>();
+        map.entrySet().stream().forEach(e->rev.put(e.getValue(),e.getKey()));
+        return rev;
     }
-
-
-
 }
